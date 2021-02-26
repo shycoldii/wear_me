5cc9883f8b8bd1f546ef1dc8d6bf57b44912179b
shycoldii <57770001+shycoldii@users.noreply.github.com>
2021-02-26 13:50:13 +0300

    Initial commit

---
+++ src/main/java/shop/shop/ShopApplication.java
@@ -0,0 +1,11 @@
+package shop.shop;
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication  {
+    public static void main(String[] args) {
+        Application.launch(JavaFxApplication.class, args);
+    }
+
+}
